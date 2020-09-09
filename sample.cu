c6ec683ac2d09c9e13aeadc898584aa751ad1135
Silja Maria Knudsen <smk@schultz.dk>
2020-09-09 13:39:50 +0200

    timeval

--- sample.cu
+++ sample.cu
@@ -21,6 +21,14 @@ __global__ void squareKernel(float* d_in, float* d_out, int N){
 }
 
 
+int timeval_substract(struct timeval* result, struct timeval* t2, struct timeval* t1){
+    unsigned int resolution = 1000000; 
+    long int diff = (t2 -> tv_usec + resolution * t2 -> tv_sec) - (t1 -> tv_usec + resolution * t1 -> tv_sec);
+    result -> tv_sec = diff/resolution; 
+    result -> tv_usec = diff % resolution; 
+    return (diff <0); 
+}
+
 int main(int argc, char** argv){
     unsigned int N = 753411; //størrelsen på arrayet
     unsigned int mem_size = N*sizeof(float); //størrelsen på hukommelsen der skal bruges til arrayet
