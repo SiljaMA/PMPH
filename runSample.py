688f982571d5fd217fd0e9950406d3d4cad4ce98
Silja Maria Knudsen <smk@schultz.dk>
2020-09-09 21:28:17 +0200

    Opgave 2

--- runSample.py
+++ runSample.py
@@ -3,7 +3,7 @@ import subprocess
 cmd = "./a.out"
 
 #Just trying to find the sweet spot, when the gpu is faster than the cpu
-for i in range(200,1000): 
+for i in range(2000,2500): 
     o = subprocess.call([cmd, i])
     print(o.stdout)
 
