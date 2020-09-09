e949600cae6556a3f362fc86f83aee25b2a13d94
Silja Maria Knudsen <smk@schultz.dk>
2020-09-09 16:58:00 +0200

    timeval

--- sample.cu
+++ sample.cu
@@ -37,11 +37,9 @@ int main(int argc, char** argv){
     unsigned int mem_size = N*sizeof(float); //størrelsen på hukommelsen der skal bruges til arrayet
     unsigned int block_size = 256; //størrelsen på en block
     unsigned int num_blocks = ((N + (block_size -1))/block_size); //antallet af blocks
-
-
     //For measure the time 
-    unsigned long int elaped; struct timeval t_start, t_end, t_diff; 
-    gettimeofday(&t_start, NULL); 
+    unsigned long int elapsed_gpu; struct timeval t_start_gpu, t_end_gpu, t_diff_gpu;  
+    unsigned long int elapsed_cpu; struct timeval t_start_cpu, t_end_cpu, t_diff_cpu;  
 
     //allocates host-memory
     float* h_in = (float*) malloc(mem_size);
@@ -61,10 +59,10 @@ int main(int argc, char** argv){
     //copy host memory to device
     cudaMemcpy(d_in, h_in, mem_size, cudaMemcpyHostToDevice);
 
+    gettimeofday(&t_start_gpu, NULL); 
     //execute the kernel and calculates the square using gpu 
-    for(int i = 0; i < GPU_RUNS; i++){
-        squareKernel <<<num_blocks, block_size>>>(d_in, d_out, N);
-    }cudaThreadSynchronize(); 
+    squareKernel <<<num_blocks, block_size>>>(d_in, d_out, N);
+    gettimeofday(&t_end_gpu, NULL); 
 
     //copy result from device to host
     cudaMemcpy(h_out, d_out, mem_size, cudaMemcpyDeviceToHost);
@@ -72,12 +70,15 @@ int main(int argc, char** argv){
     //print result
     //for(unsigned int i = 0; i <N; i++) printf("%.6f\n", h_out[i]);
 
-    //Calculates squareSerial using the cpu
     float* cpu_res = (float*) malloc(mem_size);
-    squareSerial(h_in, cpu_res, N); 
 
-    //Checks the results are the same
-     
+    gettimeofday(&t_start_cpu, NULL); 
+    //Calculates squareSerial using the cpu
+    squareSerial(h_in, cpu_res, N); 
+    gettimeofday(&t_start_cpu, NULL); 
+
+
+    //Checks the results are the same     
     for (unsigned int j = 0; j < N; ++j){
         if(fabs(cpu_res[j] - h_out[j]) < 0.0001){
             printf("VALID \n");
@@ -86,16 +87,15 @@ int main(int argc, char** argv){
         }
     }
 
-    gettimeofday(&t_diff, NULL); 
-    timeval_substract(&t_diff, &t_end, &t_start); 
-    elaped = (t_diff.tv_sec*1e6+t_diff.tv_usec)/GPU_RUNS; 
-    printf("Took %d microseconds (%.2fms)\n", elaped, elaped/1000.0);
-
-    
-
-    //mål tiden 
-    //undersøg hvornår gpuen bliver hurtigere 
-
+    //time for kernel gpu
+    timeval_substract(&t_diff_gpu, &t_end_gpu, &t_start_gpu); 
+    elapsed_gpu = (t_diff_gpu.tv_sec*1e6+t_diff_gpu.tv_usec); 
+    printf("GPU took %d microseconds (%.2fms)\n", elapsed_gpu, elapsed_gpu/1000.0);
+  
+    //Time for serial on cpu
+    timeval_substract(&t_diff_cpu, &t_end_cpu, &t_start_cpu); 
+    elapsed_cpu = (t_diff_cpu.tv_sec*1e6+t_diff_cpu.tv_usec); 
+    printf("CPU took %d microseconds (%.2fms)\n", elapsed_cpu, elapsed_cpu/1000.0);
 
     //clean-up memory
     free(h_in);
