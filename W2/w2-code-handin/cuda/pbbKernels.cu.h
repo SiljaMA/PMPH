0ad6eb4e0edce89b02401a3871cad5bf4db7cbc9
Silja Maria Knudsen <smk@schultz.dk>
2020-09-16 22:15:39 +0200

    l

--- W2/w2-code-handin/cuda/pbbKernels.cu.h
+++ W2/w2-code-handin/cuda/pbbKernels.cu.h
@@ -184,7 +184,7 @@ scanIncWarp( volatile typename OP::RedElTp* ptr, const unsigned int idx ) {
     for(unsigned int d = 0; d < lgWARP; d++){
         unsigned int h = pow(2,d); 
         if (lane >= h){
-            ptr[idx] = OP:: apply ptr[idx - h] ptr[idx];  
+            ptr[idx] = OP::apply(ptr[idx - h], ptr[idx]);  
         }
     }
 
